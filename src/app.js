4269068aff99b63c7f4c9d415d4dc831ce4a15e1
ksaneedh-max <ksaneedh@gmail.com>
2026-03-15 18:30:40 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const loginRoutes = require("./modules/login/login.route");
+const logoutRoutes = require("./modules/logout/logout.route");
 const attendanceRoutes = require("./modules/attendance/attendance.route");
 const marksRoutes = require("./modules/marks/marks.route");
 
@@ -9,16 +10,23 @@ const app = express();
 app.use(express.json());
 app.use(express.static("public"));
 
+/* ---------- REQUEST LOGGER ---------- */
+
 app.use((req, res, next) => {
   console.log(new Date().toISOString(), req.method, req.url);
   next();
 });
 
+/* ---------- HEALTH CHECK ---------- */
+
 app.get("/", (req, res) => {
   res.send("SRM Dashboard API is running");
 });
 
+/* ---------- ROUTES ---------- */
+
 app.use("/login", loginRoutes);
+app.use("/logout", logoutRoutes);
 app.use("/attendance", attendanceRoutes);
 app.use("/marks", marksRoutes);
 
